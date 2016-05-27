ed0a403e4eae7bd369fc96ed2bb38e49f5a71abd
ravelasquez99 <ravelasquez99@gmail.com>
2016-05-27 07:37:30 -0700

    Microsoft SDK installed

--- HDIL/HDIL/HomeVC.swift
+++ HDIL/HDIL/HomeVC.swift
@@ -8,17 +8,46 @@
 
 import UIKit
 
-class HomeVC: UIViewController {
+class HomeVC: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
   
+  @IBOutlet var HDILButton: UIButton!
   @IBOutlet var refreshButton: UIBarButtonItem!
-  @IBOutlet weak var hdilButton: NSLayoutConstraint!
+  @IBOutlet var imageView: UIImageView!
+  
+  @IBOutlet var chooseImageButton: UIButton!
+  var imagePicker : UIImagePickerController!
   
   override func viewDidLoad() {
     self.refreshButton.enabled = false
     self.refreshButton.tintColor = UIColor.clearColor()
   }
   
+ 
+  @IBAction func ChoosePicture(sender: UIButton) {
+    
+    self.imagePicker = UIImagePickerController()
+    self.imagePicker.delegate = self
+    self.imagePicker.sourceType = .SavedPhotosAlbum
+    self.presentViewController(self.imagePicker, animated: true, completion: nil)
+  }
+  
+  @IBAction func onHDILPressed(sender: UIButton) {
+  self.imagePicker = UIImagePickerController()
+  self.imagePicker.delegate = self
+  self.imagePicker.sourceType = .Camera
+  self.presentViewController(self.imagePicker, animated: true, completion: nil)
+    
+  }
+  
+  
+  func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+    
+    picker.dismissViewControllerAnimated(true, completion: nil)
+    imageView.image = info[UIImagePickerControllerOriginalImage] as? UIImage
+    self.HDILButton.setTitle("Try Again", forState: .Normal)
+    self.chooseImageButton.setTitle("Choose a new picture", forState: .Normal)
+  }
 
 
   
